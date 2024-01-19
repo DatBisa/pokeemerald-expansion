cb5a658cc8e665beb3902cb0d193d465305114b5
DatBisa <heidecker98@gmail.com>
2024-01-19 13:03:47 +0100

    bulk map update

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,3 +1059,17 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/Underwater_Route102/scripts.inc"
+
+	.include "data/maps/OldaleSprings_Entrance/scripts.inc"
+
+	.include "data/maps/Underwater_OldaleSprings/scripts.inc"
+
+	.include "data/maps/LittlerootTown_House1/scripts.inc"
+
+	.include "data/maps/LittlerootShores/scripts.inc"
+
+	.include "data/maps/PetalburgShores/scripts.inc"
+
+	.include "data/maps/RustboroShores/scripts.inc"
